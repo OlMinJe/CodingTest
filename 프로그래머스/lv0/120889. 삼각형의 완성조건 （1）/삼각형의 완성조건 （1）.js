b54c7a26a51c0ae7458685dd27c339d5d470f68b
이민제 <118544134+OlMinJe@users.noreply.github.com>
2023-04-08 23:01:15 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,9 @@
+function solution(sides) {    
+    sides.sort(function(a, b)  {
+        if(a > b) return 1;
+        if(a < b) return -1;
+        return 0;
+    });
+    
+    return (sides[2] < sides[0] + sides[1]) ? 1 : 2;
+}
