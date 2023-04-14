38a409b760e5410f087c7106afc07cf28820d420
이민제 <118544134+OlMinJe@users.noreply.github.com>
2023-04-14 14:46:49 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,9 +1,5 @@
-function solution(my_string) {
-    let answer = [];
-
-    for(let i = 0; i < my_string.length; i++) {
-        my_string[i] === my_string[i].toUpperCase() ? 
-            answer.push(my_string[i].toLowerCase()) : answer.push(my_string[i].toUpperCase()); 
-	}
-    return answer.join("");
+function solution(my_string) {    
+    return my_string.split("")
+        .map(e => e === e.toUpperCase() ? e.toLowerCase() : e.toUpperCase())
+        .join("");
 }
