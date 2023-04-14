ac1c9b559419c8516b99fa3c530fd8d7f65f2f27
이민제 <118544134+OlMinJe@users.noreply.github.com>
2023-04-14 14:43:24 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.35 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let answer = [];
+
+    for(let i = 0; i < my_string.length; i++) {
+        my_string[i] === my_string[i].toUpperCase() ? 
+            answer.push(my_string[i].toLowerCase()) : answer.push(my_string[i].toUpperCase()); 
+	}
+    return answer.join("");
+}
