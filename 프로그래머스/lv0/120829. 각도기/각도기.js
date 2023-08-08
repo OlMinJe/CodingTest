873b112a0aca8739f84dea66ec68a62a0e711663
OlMinJE <Ol.MinJe@gmail.com>
2023-08-08 22:12:18 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,4 +1,1 @@
-function solution(angle) {
-
-    return angle == 180 ? 4 : (angle > 90 ? 3 : (angle == 90 ? 2 : (angle > 0 ? 1 : -1)));
-}
+const solution = (angle) => [0, 90, 91, 180].filter(x => angle>=x).length;
