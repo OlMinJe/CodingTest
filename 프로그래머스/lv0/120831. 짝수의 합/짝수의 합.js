31a7a7cac0baa42656fb21bd38fa0e39565a75a6
OlMinJE <Ol.MinJe@gmail.com>
2023-08-08 23:30:35 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,11 +1,8 @@
-function solution(n) {
-    var answer = 0;
-    
-    for(let i = 0; i <= n; i++){
-        if(i % 2 == 0){
-            answer += i;
-        }
-    }
-    
+const solution = (n) => {
+    let answer = 0;
+
+    for (let i = 0; i <= n; i += 2) 
+        answer += i;
+
     return answer;
 }
