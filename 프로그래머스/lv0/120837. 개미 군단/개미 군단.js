a3a906397a81f0c11510f2454c4e7f4af0fc7538
OlMinJE <Ol.MinJe@gmail.com>
2023-08-13 01:18:39 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,3 +1,1 @@
-function solution(hp) {
-    return Math.floor(hp/5) + Math.floor(hp%5/3) + Math.floor(hp%5%3);
-}
+const solution = hp => Math.floor(hp/5) + Math.floor(hp%5/3) + Math.floor(hp%5%3);
