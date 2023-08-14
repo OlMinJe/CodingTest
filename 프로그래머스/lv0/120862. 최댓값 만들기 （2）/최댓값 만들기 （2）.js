d3e7357971035892a06dba79e9de84fc7c09041f
OlMinJE <Ol.MinJe@gmail.com>
2023-08-14 18:06:25 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,9 +1,4 @@
-function solution(numbers) {
-    let answer = [];
-    for(let i = 0; i < numbers.length - 1; i++){
-        for(let j = i + 1; j < numbers.length; j++){
-            answer.push(numbers[i] * numbers[j]);
-        }
-    }
-    return Math.max(...answer);
+const solution = numbers => {
+    let num = [...numbers.sort((a,b) => a-b)];
+    return Math.max(num[0]*num[1], num[num.length-1]*num[num.length-2]);
 }
