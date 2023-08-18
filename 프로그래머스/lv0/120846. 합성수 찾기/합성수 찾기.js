0f49b54d28680ffdd235c883e545a6f1656f7dad
OlMinJE <Ol.MinJe@gmail.com>
2023-08-18 16:51:56 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,13 +1,15 @@
-function solution(n) {
-    var answer = 0;
-    
-    for(let i = 1; i <= n; i++) {
-        let cnt = 0;
-        for(let j = 0; j <= i; j++) {
-            if(i % j === 0) cnt ++;
-        }
-        if(cnt > 2) answer += 1;
+const solution = n => {
+    let answer = 0;
+
+    for (let i = 4; i <= n; i++) {
+        let limit = Math.floor(Math.sqrt(i));
+        for (let j = 2; j <= limit; j++) {
+						if (i % j === 0) {
+                answer++;
+                break;
+            }
+				}
     }
-    
+
     return answer;
 }
