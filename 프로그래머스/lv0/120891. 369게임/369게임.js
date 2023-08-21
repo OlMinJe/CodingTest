e8851fcdae512dc17fac1fb9ec9e3d3728685777
OlMinJE <Ol.MinJe@gmail.com>
2023-08-21 19:58:18 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,6 +1,1 @@
-function solution(order) {
-    return order.toString()
-        .split('')
-        .filter(e => Number(e) % 3 === 0 && Number(e) != 0)
-        .length;
-}
+const solution = order => order.toString().split('').filter(e => Number(e)%3===0 && Number(e)!=0).length;
