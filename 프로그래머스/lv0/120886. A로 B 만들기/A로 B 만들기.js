7915aff900af8c1b3065683ee38379f81abdc0f7
OlMinJE <Ol.MinJe@gmail.com>
2023-08-21 20:22:39 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,3 +1,1 @@
-function solution(before, after) {
-    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0;
-}
+const solution = (before, after) => [...before].sort().join('') === [...after].sort().join('') ? 1 : 0;
