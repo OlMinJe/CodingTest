23d8fd30489c920b6686f9fd0f7b0f0d5ea9d6ee
OlMinJE <Ol.MinJe@gmail.com>
2023-09-08 22:54:08 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,11 +1,1 @@
-function solution(A, B) {
-    let arr = [...A]
-    for (let i = 0; i< arr.length; i++) {
-        if(A === B) return 0;
-        else {
-            arr.unshift(arr.pop())
-            if (arr.join('') === B) return i+1;
-        }
-    }
-    return -1;
-}
+const solution = (a,b) => (b+b).indexOf(a);
