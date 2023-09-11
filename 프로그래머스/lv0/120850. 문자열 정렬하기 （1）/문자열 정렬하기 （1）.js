da544fd8e10d5659065853771407af4aa78f6dae
OlMinJE <Ol.MinJe@gmail.com>
2023-09-11 20:21:46 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,6 +1,4 @@
-function solution(my_string) { 
-    return my_string.replace(/[a-z]/gi, '')
-            .split("")
-            .sort((a, b) => a - b)
-            .map(e => Number(e));
-}
+const solution = my_string => Array.from(my_string)
+                                .filter(e => !isNaN(+e))
+                                .sort((a,b) => a-b)
+                                .map(e => +e);
