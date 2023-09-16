7ea34fedf0c3e84042d8dae88248dc547ab145b8
OlMinJE <Ol.MinJe@gmail.com>
2023-09-16 16:44:38 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,7 +1,1 @@
-function solution(money) {
-    const americano = 5500;
-    const num = Math.floor(money / americano);
-    const change = money % americano;
-    let answer = [num, change];
-    return answer;
-}
+const solution = money => [Math.floor(money / 5500), money % 5500];
