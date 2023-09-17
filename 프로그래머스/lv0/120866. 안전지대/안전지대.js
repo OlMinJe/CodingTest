e024d8e39896d7c905724efae357c2d207eb4c19
OlMinJE <Ol.MinJe@gmail.com>
2023-09-17 21:52:42 +0900

    [level 0] Title: 안전지대, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,23 +1,17 @@
 const solution = board => {
-    const coordinate = [[-1, -1], [-1, 0], [-1, 1],
-                        [0, -1], [0, 1],
-                        [1, -1], [1, 0], [1, 1]];
+    let coordinate = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
     let cntTotal = 0;
     
-    board.forEach((row, i) => {
-        row.forEach((cell, j) => {
-            if (cell===0) {
-                let isFine = true;
-
-                coordinate.forEach(([dx, dy]) => {
-                    const [nx, ny] = [i+dx, j+dy];
-
-                    if (nx>=0 && nx<board.length && ny>=0 && ny<board[0].length && board[nx][ny]===1) isFine = false;
-                });
-                if (isFine) cntTotal++;
-            }
-        });
-    });
+    board.forEach((row, i) => row.forEach((cell, j) => {
+            if (cell===1) return false;
+            return coordinate
+                .some(([dx, dy]) => (i+dx>=0 
+                                        && i+dx<board.length 
+                                        && j+dy>=0 
+                                        && j+dy<board[0].length 
+                                        && board[i+dx][j+dy]===1)) ? false : cntTotal++;
+        })
+    );
 
     return cntTotal;
 }
