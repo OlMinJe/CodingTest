b61a35f2c98d311217711db367563a6df1c49358
OlMinJE <Ol.MinJe@gmail.com>
2023-09-17 21:29:45 +0900

    [level 0] Title: 안전지대, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,32 +1,23 @@
-function solution(board) {
-    const coordinate = [
-        [-1, -1], [-1, 0], [-1, 1],
-        [0, -1], [0, 1],
-        [1, -1], [1, 0], [1, 1]
-    ];
-
-    let cntLandMine = 0;
-
+const solution = board => {
+    const coordinate = [[-1, -1], [-1, 0], [-1, 1],
+                        [0, -1], [0, 1],
+                        [1, -1], [1, 0], [1, 1]];
+    let cntTotal = 0;
+    
     board.forEach((row, i) => {
         row.forEach((cell, j) => {
-            if (cell === 0) {
-                let isLandMine = true;
+            if (cell===0) {
+                let isFine = true;
 
                 coordinate.forEach(([dx, dy]) => {
-                    const nx = i + dx;
-                    const ny = j + dy;
+                    const [nx, ny] = [i+dx, j+dy];
 
-                    if (nx >= 0 
-                        && nx < board.length 
-                        && ny >= 0 
-                        && ny < board[0].length 
-                        && board[nx][ny] === 1) isLandMine = false;
+                    if (nx>=0 && nx<board.length && ny>=0 && ny<board[0].length && board[nx][ny]===1) isFine = false;
                 });
-
-                if (isLandMine) cntLandMine++;
+                if (isFine) cntTotal++;
             }
         });
     });
 
-    return cntLandMine;
+    return cntTotal;
 }
