79189e5774b4da330af65c035c6a856a83ec14f7
OlMinJE <Ol.MinJe@gmail.com>
2023-09-20 17:58:14 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,3 +1,5 @@
-function solution(a, b) {
-    return (a/b).toString().length > 11 ? 2 : 1;
-}
+const solution = (a, b) => {
+    while (b%2 === 0) b/=2;
+    while (b%5 === 0) b/=5;
+    return b===1 ? 1 : (a%b === 0 ? 1 : 2);
+};
