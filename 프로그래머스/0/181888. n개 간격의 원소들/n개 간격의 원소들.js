66f5c684117420680051f04c6e19d3012555d088
OlMinJe <Ol.MinJe@gmail.com>
2024-02-16 16:09:11 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,11 +1,1 @@
-const solution = (num_list, n) => {
-    const resultArr = [];
-    
-    let i = 0
-    while (i < num_list.length) {
-        resultArr.push(num_list[i]);
-        i += n;
-    }
-    
-    return resultArr;
-};
+const solution = (num_list, n) => num_list.filter((e, i) =>  i === 0 || i % n === 0 ? e : '');
