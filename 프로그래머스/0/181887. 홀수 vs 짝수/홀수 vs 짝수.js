c1178a9bc078f24774ee5278ba5ee86aae2c3241
OlMinJe <Ol.MinJe@gmail.com>
2024-02-17 00:41:35 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,6 +1,6 @@
 const solution = (num_list) => {
-    const evenArr = num_list.filter((e, i) => i % 2 === 0).reduce((acc, cur) => acc + cur);
-    const oddArr = num_list.filter((e, i) => i % 2 !== 0).reduce((acc, cur) => acc + cur);
+    const evenArr = num_list.reduce((acc, cur, i) => i % 2 === 0 ? acc : acc + cur, 0);
+    const oddArr = num_list.reduce((acc, cur, i) => i % 2 === 0 ? acc + cur : acc, 0);
     
     return evenArr >= oddArr ? evenArr : oddArr;
 }
