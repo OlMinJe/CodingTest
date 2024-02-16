1128168d61d2ee260e3e57b6415a30806969f754
OlMinJe <Ol.MinJe@gmail.com>
2024-02-16 17:15:13 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,4 +1,4 @@
-const solution = (num_list) => {
-    const evenArr = num_list.filter((e) => e % 2 === 0).length;
-    return [evenArr, num_list.length - evenArr];
-}
+const solution = num_list => num_list.reduce((res, n) => {
+	res[n % 2 === 0 ? 0 : 1] += 1;
+	return res;
+}, [0, 0]);
