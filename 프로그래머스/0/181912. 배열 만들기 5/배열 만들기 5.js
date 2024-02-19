83b7280f5cafef4b43e1566ca69aa230b539e5b9
OlMinJe <Ol.MinJe@gmail.com>
2024-02-19 16:02:11 +0900

    [level 0] Title: 배열 만들기 5, Time: 25.86 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,4 +1,10 @@
-const solution = (intStrs, k, s, l) => intStrs.map(e => {
-    const strArr = e.split('').map(el => [el]).slice(s, s + l).join('');
-    return Number(strArr);
-}).filter(e => e > k);
+const solution = (intStrs, k, s, l) =>
+  intStrs
+    .map((e) => {
+      const strArr = [...e]
+        .map((el) => [el])
+        .slice(s, s + l)
+        .join('');
+      return Number(strArr);
+    })
+    .filter((e) => e > k);
