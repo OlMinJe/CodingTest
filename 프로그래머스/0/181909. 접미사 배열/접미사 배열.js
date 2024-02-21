f811b0d33cdba63686d21cb5249333cf2d302fd7
OlMinJe <Ol.MinJe@gmail.com>
2024-02-21 17:12:38 +0900

    접미사 배열 다시 풀어보기

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,9 +1,1 @@
-const solution = (my_string) => {
-    const strLen = my_string.length;
-    const suffix = [...my_string].reduce((acc, cur, i) => {
-        const customCur = my_string.slice(i, strLen);
-        return [...acc, customCur];
-    }, []);
-    
-    return suffix.sort();
-}
+const solution = str => Array.from({length: str.length}, (_, i) => str.slice(i)).sort();
