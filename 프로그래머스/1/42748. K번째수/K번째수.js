acdb0b509b4d2dff2043cd9e858986b9065b2537
OlMinJe <Ol.MinJe@gmail.com>
2025-07-10 16:09:09 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,5 +1,12 @@
-// 풀이 1
-const solution = (array, commands) => 
-    commands.map(([i, j, k]) => array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]);
+// 풀이
+function solution(array, commands) {
+    
+  return commands.map(([i, j, k]) => {
+    return array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+  });
+}
 
-// 풀이 2
+// 정리
+// const solution = (array, commands) => 
+//     commands.map(([i, j, k]) => array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]);
+
