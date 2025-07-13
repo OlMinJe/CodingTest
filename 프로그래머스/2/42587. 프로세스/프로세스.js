b79c411ce3953f7452c7cde955ac41a3ff89aaf7
OlMinJe <Ol.MinJe@gmail.com>
2025-07-13 23:29:39 +0900

    [level 2] Title: 프로세스, Time: 1.28 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,16 @@
+function solution(priorities, location) {
+  const queue = priorities.map((priority, index) => ({ index, priority }));
+  let count = 0;
+
+  while (queue.length) {
+    const current = queue.shift();
+    const maxPriority = Math.max(...queue.map(item => item.priority));
+
+    if (current.priority < maxPriority) {
+      queue.push(current);
+    } else {
+      count++;
+      if (current.index === location) return count;
+    }
+  }
+}
