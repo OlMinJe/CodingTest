99564fc73abbeed70b62eeae1be55b4d72d22b00
OlMinJe <Ol.MinJe@gmail.com>
2025-07-16 09:12:56 +0900

    [level 2] Title: 의상, Time: 0.29 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,12 @@
+function solution(clothes) {
+    const clothesMap = new Map();
+
+    for (const [name, type] of clothes) {
+        clothesMap.set(type, (clothesMap.get(type) || 0) + 1);
+    }
+
+    const answer = [...clothesMap.values()]
+        .reduce((acc, count) => acc * (count + 1), 1);
+
+    return answer - 1;
+}
