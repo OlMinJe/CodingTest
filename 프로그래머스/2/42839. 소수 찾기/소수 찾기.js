1f0cf0493426c0eb5cb59c3bf790dedc673ad7b1
OlMinJe <Ol.MinJe@gmail.com>
2025-07-21 14:14:11 +0900

    [level 2] Title: 소수 찾기, Time: 23.73 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,34 @@
+function solution(numbers) {
+  const numPieces = numbers.split('');
+  const numberSet = new Set();
+
+  const getPermutations = (arr, len, current = '') => {
+    if (current.length === len) {
+      numberSet.add(Number(current));
+      return;
+    }
+
+    arr.forEach((fixed, idx) => {
+      const next = [...arr];
+      next.splice(idx, 1);
+      getPermutations(next, len, current + arr[idx]);
+    });
+  };
+
+  for (let i = 1; i <= numPieces.length; i++) {
+    getPermutations(numPieces, i);
+  }
+
+  const isPrime = (n) => {
+    if (n === 2) return true;
+    if (n < 2 || n % 2 === 0) return false;
+
+    const sqrt = Math.sqrt(n);
+    for (let i = 3; i <= sqrt; i += 2) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  };
+
+  return [...numberSet].filter(isPrime).length;
+}
