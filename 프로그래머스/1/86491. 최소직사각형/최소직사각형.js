81785ee5395af45a2ed70e10b5dbbc355e3d7e3f
OlMinJe <Ol.MinJe@gmail.com>
2025-07-21 16:01:15 +0900

    [level 1] Title: 최소직사각형, Time: 32.76 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -3,8 +3,7 @@ function solution(sizes) {
     let maxHeight = 0;
     
     for (let [w, h] of sizes) {
-        const big = Math.max(w, h);
-        const small = Math.min(w, h);
+        const [big, small] = w > h ? [w, h] : [h, w]
         
         maxWidth = Math.max(maxWidth, big);
         maxHeight = Math.max(maxHeight, small)
