0ddcabf0cf6553b19a2ad119a7576fd8ddffcddb
OlMinJe <Ol.MinJe@gmail.com>
2025-07-23 16:40:01 +0900

    [level 2] Title: 모음 사전, Time: 4.21 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,24 +1,46 @@
-const FIXED_WORD = ['A', 'E', 'I', 'O', 'U'];
 
-const calcWeights = (length = 5) => {
-  const weight = Array(length).fill(0);
-  weight[length - 1] = 1;
+// const WEIGHT_WORD = ['A', 'E', 'I', 'O', 'U'];
 
-  for (let i = length - 2; i >= 0; i--) {
-    weight[i] = weight[i + 1] * FIXED_WORD.length + 1;
-  }
+// const calcWeights = (length = 5) => {
+//   const weightArray = Array(length).fill(0);
+//   weightArray[length - 1] = 1;
 
-  return weight;
-};
+//   for (let i = length - 2; i >= 0; i--) {
+//     weightArray[i] = weightArray[i + 1] * WEIGHT_WORD.length + 1;
+//   }
+
+//   return weightArray;
+// };
+
+// function solution(word) {
+//   const weight = calcWeights();
+//   let answer = 0;
+
+//   for (let i = 0; i < word.length; i++) {
+//     const charIndex = WEIGHT_WORD.indexOf(word[i]);
+//     answer += charIndex * weight[i] + 1;
+//   }
+
+//   return answer;
+// }
+
 
 function solution(word) {
-  const weight = calcWeights();
-  let answer = 0;
+    const dict = [];
+    const letters = ['A', 'E', 'I', 'O', 'U'];
 
-  for (let i = 0; i < word.length; i++) {
-    const charIndex = FIXED_WORD.indexOf(word[i]);
-    answer += charIndex * weight[i] + 1;
-  }
+    function dfs(current) {
+        if (current.length > 5) return;
 
-  return answer;
+        if (current.length > 0) dict.push(current);
+
+        for (let i = 0; i < letters.length; i++) {
+            dfs(current + letters[i]);
+        }
+    }
+
+    dfs("");
+    
+    
+    return dict.indexOf(word) + 1;
 }
