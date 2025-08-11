fad4506cd1bae89fc8f1f03281f007fb840885b7
OlMinJe <Ol.MinJe@gmail.com>
2025-08-11 15:02:45 +0900

    [level 3] Title: N으로 표현, Time: 43.61 ms, Memory: 44.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.js
@@ -1,49 +1,27 @@
 function solution(N, number) {
-    if (N === number) return 1;
-    
-    const LIMIT = 32000 * 2
-    const minimum = new Map();
-    const dp = Array.from({ length: 9 }, () => new Set());
-    
-    let concat = 0;
-    
-    const inRange = (x) => LIMIT === 0 ? true : (x >= -LIMIT && x <= LIMIT);
-    
-    const tryAdd = (set, val, cost) => {
-        if (!inRange(val)) return;
-        
-        const prev = minimum.get(val);
-        if (prev !== undefined && prev <= cost) return;
-        
-        minimum.set(val, cost);
-        set.add(val);
-    }
-    
-    for (let i = 1; i <= 8; i++) {
-        concat = concat * 10 + N;
-        tryAdd(dp[i], concat, i);
-        
-        if (concat === number) return i;
-        
-        for (let j = 1; j < i; j++) {
-            const A = dp[j];
-            const B = dp[i - j];
-            
-            for (const a of A) {
-                for (const b of B) {
-                    if (a <= b) tryAdd(dp[i], a + b, i);
-                    tryAdd(dp[i], a - b, i); 
-                    tryAdd(dp[i], b - a, i);
-                    
-                    if (a <= b) tryAdd(dp[i], a * b, i);
-                    if (b !== 0) tryAdd(dp[i], Math.trunc(a / b), i);
-                    if (a !== 0) tryAdd(dp[i], Math.trunc(b / a), i);
+    let dp = Array.from({length : 9}, () => new Set());
+    for(let i = 1 ; i <= 8 ;i++) {
+        let stringN = '';
+
+        for(let j = 0 ; j < i ; j++) {
+            stringN += String(N);
+        }
+
+        dp[i].add(Number(stringN));
+
+        for(let k = 1 ; k < i ; k++) {
+            for(const a of dp[k]) {
+                for(const b of dp[i-k]) {
+                    dp[i].add(a+b);
+                    dp[i].add(a-b);
+                    dp[i].add(a*b);
+                    dp[i].add(a/b);
                 }
             }
         }
-        
-        if (dp[i].has(number)) return i;
+
+        if(dp[i].has(number)) return i
     }
 
-  return -1;
+    return -1;
 }
